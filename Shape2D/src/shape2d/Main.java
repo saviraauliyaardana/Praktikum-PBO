39473f0e2e5fe41dad6b923348758c5927b2c345
Savira Auliya Ardana <savirauliya10@gmail.com>
2022-11-28 15:39:39 +0700

    feat : add jframe

--- Shape2D/src/shape2d/Main.java
+++ Shape2D/src/shape2d/Main.java
@@ -1,8 +1,13 @@
 package shape2d;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class Main {
+public class Main extends JFrame{
 
     public static void main(String[] args) {
         try {
@@ -30,6 +35,28 @@ public class Main {
             LimasSegitigaSamaSisi limas = new LimasSegitigaSamaSisi();
             System.out.println(limas.luas(8, 7, 4));
             System.out.println(limas.volume(9, 3));
+            
+            Main frame = new Main();
+            frame.setBounds(10, 10, 1000, 500);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(Main.DISPOSE_ON_CLOSE);
+            frame.setLayout(null);
+            
+            JTextField alas = new JTextField();
+            alas.setBounds(5, 5, 100, 50);
+            alas.setText("0");
+            frame.add(alas);
+            
+            JButton hitung = new JButton("hitung");
+            hitung.setBounds(115, 5, 100, 50);
+            frame.add(hitung);
+            
+            hitung.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    System.out.println(alas.getText());
+                }
+            });
             
         } catch (Exception e) {
             System.out.println("Input tidak sesuai");
