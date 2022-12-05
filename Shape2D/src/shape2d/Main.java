e7f5a7dd370a99590f9a11ac07fa7f106eff5378
Savira Auliya Ardana <savirauliya10@gmail.com>
2022-12-05 15:30:51 +0700

    feat : init input

--- Shape2D/src/shape2d/Main.java
+++ Shape2D/src/shape2d/Main.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class Main extends JFrame{
@@ -47,15 +48,25 @@ public class Main extends JFrame{
             alas.setText("0");
             frame.add(alas);
             
+            JTextField tinggi = new JTextField();
+            tinggi.setBounds(5, 65, 100, 50);
+            tinggi.setText("0");
+            frame.add(tinggi);
+            
             JButton hitung = new JButton("hitung");
             hitung.setBounds(115, 5, 100, 50);
             frame.add(hitung);
             
-            hitung.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent arg0) {
-                    System.out.println(alas.getText());
-                }
+            JLabel hasil = new JLabel("0");
+            hasil.setBounds(115, 65, 100, 50);
+            frame.add(hasil);
+            
+            hitung.addActionListener((ActionEvent arg0) -> {
+                double alas_val = Double.parseDouble(alas.getText());
+                double tinggi_val = Double.parseDouble(tinggi.getText());
+                Segitiga segitiga1 = new Segitiga();
+                double hasil_val = segitiga1.luas(alas_val, tinggi_val);
+                hasil.setText(String.valueOf(hasil_val));
             });
             
         } catch (Exception e) {
